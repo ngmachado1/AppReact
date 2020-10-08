d4138cc2e74d0956f3466c8d78e4fb07ad1ecee1
ngmachado1 <54647104+ngmachado1@users.noreply.github.com>
2020-10-07 22:18:20 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+//importar un componente
+import Img from './components/img'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Img />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> Giselle MAchado
         </p>
         <a
           className="App-link"
